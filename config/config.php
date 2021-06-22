4d173f4f0ee727e5dc678d384b79f1335d54b788
Snehal Bera <snehalbera@gmail.com>
2021-06-22 23:50:22 +0530

    updated config

--- config/config.php
+++ config/config.php
@@ -3,8 +3,7 @@
 	$timezone = date_default_timezone_set("Asia/Kolkata");
 
 	// DATA SOURCE NETWORK (DATABASE CONNECTION)
-	// -----------------------------------------------------------
-	$con = mysqli_connect('localhost', 'root', '', 'github cms');
+	$con = mysqli_connect('localhost', 'root', '', 'blogsite');
 	if(mysqli_errno($con)){
 		echo "Failed to connect database: " . mysqli_errno($con);
 	}
